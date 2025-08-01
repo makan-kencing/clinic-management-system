d9efbf4b026ab021fc77f332934cc9bd34004660
makan-kencing <dizzynight2096@gmail.com>
2025-08-01 14:45:44 +0800

    feat: update the interface methods and provide documentations

--- src/main/java/edu/dsa/clinic/adt/ListInterface.java
+++ src/main/java/edu/dsa/clinic/adt/ListInterface.java
@@ -1,19 +1,100 @@
 package edu.dsa.clinic.adt;
 
-import java.util.Comparator;
-public interface ListInterface<T> extends QueueLikeInterface<T>, Cloneable, Iterable<T> {
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Range;
 
-    void insert(int index, T e);
-    void remove(T e);
-    T get(int index);
-    T getLast();
-    T pop();
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+
+public interface ListInterface<T> extends QueueLikeInterface<T>, Cloneable, Iterable<T> {
+    /**
+     * Insert elements after the element located at index.
+     *
+     * @param index The index to insert the element after.
+     * @param e The new element to be added.
+     */
+    void insert(@Range(from = 0, to = Integer.MAX_VALUE) int index, T e);
+
+    /**
+     * Remove the element at the specified index.
+     *
+     * @param index The index of the element to remove.
+     * @throws IndexOutOfBoundsException If the index exceed the list current size.
+     */
+    void remove(@Range(from = 0, to = Integer.MAX_VALUE) int index) throws IndexOutOfBoundsException;
+
+    /**
+     * Get the element at the specified index.
+     *
+     * @param index The index of the element to retrieve.
+     * @return The element at the specified index.
+     * @throws IndexOutOfBoundsException If the index exceed the list current size.
+     */
+    T get(@Range(from = 0, to = Integer.MAX_VALUE) int index) throws IndexOutOfBoundsException;
+
+    /**
+     * Get the last element in the list.
+     *
+     * @return The element in the last. If the element doesn't exist, return null.
+     */
+    @Nullable T getLast();
+
+    /**
+     * Remove and get the last element in the list.
+     *
+     * @return The element in the last.
+     * @throws IndexOutOfBoundsException If the element doesn't exist.
+     */
+    T pop() throws IndexOutOfBoundsException;
+
+    /**
+     * Clear the elements in the list.
+     */
     void clear();
-    int size();
-    int index(T e);
+
+    /**
+     * Get the number of elements in the list.
+     *
+     * @return The number of elements.
+     */
+    @Range(from = 0, to = Integer.MAX_VALUE) int size();
+
+    /**
+     * Get the index of the first element that matches the filter condition.
+     *
+     * @param filter The logic to filter against.
+     * @return The index of the found element.
+     * @throws NoSuchElementException If the element is not found.
+     */
+    @Range(from = 0, to = Integer.MAX_VALUE) int index(Filter<T> filter) throws NoSuchElementException;
+
+    /**
+     * Get the first element that matches the filter condition.
+     *
+     * @param filter The logic to filter against.
+     * @return The found element. If the element is not found, return null.
+     */
+    @Nullable T findFirst(Filter<T> filter);
+
+    /**
+     * Extend the current list with another list.
+     *
+     * @param other The other list to be extended with.
+     */
     void extend(ListInterface<T> other);
-    void subtract(ListInterface<T> other);
+
+    /**
+     * Filter the list based on the given logic condition.
+     *
+     * @param filter The logic to filter against.
+     */
     void filter(Filter<T> filter);
+
+    /**
+     * Sort the list based on the given comparison logic.
+     *
+     * @param sorter The comparison logic to sort against.
+     */
     void sort(Comparator<T> sorter);
 
     @FunctionalInterface
