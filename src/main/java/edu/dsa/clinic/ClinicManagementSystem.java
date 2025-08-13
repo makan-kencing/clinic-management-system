2b4c4f7c7b9b87af49b76896201cc9930b5f136f
makan-kencing <dizzynight2096@gmail.com>
2025-08-13 19:38:56 +0800

    chore: rename Main.java to ClinicManagementSystem.java

---
+++ src/main/java/edu/dsa/clinic/ClinicManagementSystem.java
@@ -0,0 +1,9 @@
+package edu.dsa.clinic;
+
+public class ClinicManagementSystem {
+    public static void main(String[] args) {
+        Initializer.initialize();
+
+        // Entrypoint to the main UI.
+    }
+}
