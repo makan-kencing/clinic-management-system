67a21b999759ea003fa13947159bb3e3729b21b6
tanyj0612 <tanyijia9558@gmail.com>
2025-08-17 23:57:02 +0800

    feat : Database at listInterface diagnosis

--- src/main/java/edu/dsa/clinic/Database.java
+++ src/main/java/edu/dsa/clinic/Database.java
@@ -2,13 +2,14 @@ package edu.dsa.clinic;
 
 import edu.dsa.clinic.adt.DoubleLinkedList;
 import edu.dsa.clinic.adt.ListInterface;
+import edu.dsa.clinic.entity.Consultation;
 import edu.dsa.clinic.entity.Diagnosis;
 import edu.dsa.clinic.entity.Patient;
 
 public class Database {
     public final static ListInterface<Patient> patientsList = new DoubleLinkedList<>();
-    private final ListInterface<Diagnosis> diagnosisList =new DoubleLinkedList<>();
-
+    public final static ListInterface<Diagnosis> diagnosisList =new DoubleLinkedList<>();
+    public final static ListInterface<Consultation> consultationList = new DoubleLinkedList<>();
     private Database() {
     }
 }
