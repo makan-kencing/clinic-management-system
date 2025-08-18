b19c61a6201060d694881a3e80b8c851418a22e2
darentks <darentks-wm23@student.tarc.edu.my>
2025-08-18 13:04:58 +0800

    added PatientController outline

---
+++ src/main/java/edu/dsa/clinic/control/PatientController.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.dsa.clinic.control;
+
+/**
+ *
+ * @author Bincent
+ */
+import edu.dsa.clinic.adt.ListInterface;
+import edu.dsa.clinic.adt.DoubleLinkedList;
+import edu.dsa.clinic.adt.SortedDoubleLinkedList;
+import edu.dsa.clinic.entity.ConsultationQueue;
+import edu.dsa.clinic.entity.Patient;
+import edu.dsa.clinic.entity.ConsultationType;
+
+public class PatientController {
+
+    private ListInterface<Patient> PatientList;
+    private ListInterface<ConsultationQueue>  ConsultationQueue;
+
+    public void createPatientRecord(){
+
+    }
+
+    public void editPatientRecord(){
+
+    }
+
+    public void removePatientRecord(){
+
+    }
+
+    public void viewPatientList(){
+
+    }
+
+    public void viewPatientDetail(){
+
+    }
+
+    public void createQueueNumber() {
+
+    }
+
+    public void removeQueueNumber() {
+
+    }
+
+    public void viewConnsultationQueue() {
+
+    }
+
+    public void viewSummaryReport() {
+
+    }
+}
