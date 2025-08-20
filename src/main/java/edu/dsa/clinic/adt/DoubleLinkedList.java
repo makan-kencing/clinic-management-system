d438cfd8c3505f3f4bcd9498f497cf018e119d70
makan-kencing <dizzynight2096@gmail.com>
2025-08-20 12:52:41 +0800

    refactor: rewrite list addition code and overall refactor + fixes #33

--- src/main/java/edu/dsa/clinic/adt/DoubleLinkedList.java
+++ src/main/java/edu/dsa/clinic/adt/DoubleLinkedList.java
@@ -8,23 +8,34 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
+/**
+ * The {@link ListInterface} implementation for this project.
+ *
+ * @implNote A double linked list is chosen due to its efficiency at insertion anywhere in the list
+ * and deletion at the head and tail of the list, in order to fulfill the Queue-ish and Stack-ish
+ * property of the {@link ListInterface}
+ *
+ * @param <T> The datatype of the element in the list.
+ * @author makan-kencing
+ */
 public class DoubleLinkedList<T> implements ListInterface<T> {
-    @Nullable
-    private Node<T> first = null;
-    @Nullable
-    private Node<T> last = null;
-    @Range(from = 0, to = Integer.MAX_VALUE)
-    private int length = 0;
-
-    protected static class Node<K> {
+    protected static class Node<K> implements Iterable<Node<K>> {
         @Nullable Node<K> before;
         @Nullable Node<K> next;
         K data;
 
+        @SuppressWarnings("unused")
+        public Node() {
+        }
+
+        public Node(K data) {
+            this.data = data;
+        }
+
         /**
          * Unlinks the current node from the chain by chaining the before and next node together.
          */
-        private void destroy() {
+        public void unlink() {
             if (this.before != null)
                 this.before.next = this.next;
 
@@ -33,13 +44,15 @@ public class DoubleLinkedList<T> implements ListInterface<T> {
         }
 
         /**
-         * Chain two nodes together.
+         * Link two nodes together.
          *
          * @param left  The left node.
          * @param right The right node.
          * @param <K>   The generic type of both nodes.
          */
-        private static <K> void chain(@Nullable Node<K> left, @Nullable Node<K> right) {
+        public static <K> void link(@Nullable Node<K> left, @Nullable Node<K> right) {
+            assert(left != right);
+
             if (left != null) {
                 left.next = right;
             }
@@ -47,46 +60,348 @@ public class DoubleLinkedList<T> implements ListInterface<T> {
                 right.before = left;
             }
         }
-    }
-
-    protected static class NodeIterator<K> implements Iterator<Node<K>>, Iterable<Node<K>> {
-        @Nullable
-        private Node<K> next;
-
-        public NodeIterator(@Nullable Node<K> startingNode) {
-            this.next = startingNode;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return this.next != null;
-        }
-
-        @Override
-        public Node<K> next() {
-            var current = this.next;
-            assert (current != null);
-
-            this.next = current.next;
-
-            return current;
-        }
 
         @Override
         public @NotNull Iterator<Node<K>> iterator() {
-            return this;
+            return new NodeIterator<>(this);
+        }
+
+        protected static class NodeIterator<K> implements Iterator<Node<K>>, Iterable<Node<K>> {
+            @Nullable
+            private Node<K> next;
+
+            public NodeIterator(@Nullable Node<K> startingNode) {
+                this.next = startingNode;
+            }
+
+            @Override
+            public boolean hasNext() {
+                return this.next != null;
+            }
+
+            @Override
+            public Node<K> next() {
+                var current = this.next;
+                assert (current != null);
+
+                this.next = current.next;
+
+                return current;
+            }
+
+            @Override
+            public @NotNull Iterator<Node<K>> iterator() {
+                return this;
+            }
         }
     }
 
-    protected static class LinkedListIterator<K> implements Iterator<K>, Iterable<K> {
-        private final NodeIterator<K> iterator;
+    protected class Reference {
+        Node<T> head;
+        Node<T> tail;
 
-        public LinkedListIterator(NodeIterator<K> nodeIterator) {
+        public Reference(Node<T> head, Node<T> tail) {
+            this.head = head;
+            this.tail = tail;
+        }
+
+        public Reference(Node<T> starting) {
+            this(starting, starting);
+        }
+    }
+
+    @Nullable
+    protected Reference reference = null;
+
+    @Range(from = 0, to = Integer.MAX_VALUE)
+    protected int length = 0;
+
+    /**
+     * Throw an {@link IndexOutOfBoundsException} if the index exceeds the length.
+     *
+     * @param index The target index to check.
+     * @throws IndexOutOfBoundsException If the index exceeds the length of the list.
+     */
+    protected void throwIfOutOfBounds(@Range(from = 0, to = Integer.MAX_VALUE) int index) throws IndexOutOfBoundsException {
+        if (index >= this.length)
+            throw new IndexOutOfBoundsException();
+    }
+
+    /**
+     * Get the {@link Node} located at the index.
+     *
+     * @param index The index of the {@link Node}
+     * @return The {@link Node} located at the index.
+     * @throws IndexOutOfBoundsException If the {@link Node} can't be found.
+     */
+    protected Node<T> getNode(int index) throws IndexOutOfBoundsException {
+        assert(this.reference != null);
+
+        var i = 0;
+        for (var node : this.reference.head)
+            if (i == index)
+                return node;
+            else
+                i++;
+
+        throw new IndexOutOfBoundsException();
+    }
+
+    /**
+     * Add a {@link Node} to the start of the list.
+     *
+     * @param node The {@link Node} to be added.
+     */
+    protected void addNodeHead(Node<T> node) {
+        if (this.reference == null)
+            this.reference = new Reference(node);
+        else {
+            Node.link(this.reference.head, node);
+            this.reference.head = node;
+        }
+    }
+
+    /**
+     * Add a {@link Node} to the end of the list.
+     *
+     * @param node The {@link Node} to be added.
+     */
+    protected void addNodeTail(Node<T> node) {
+        if (this.reference == null)
+            this.reference = new Reference(node);
+        else {
+            Node.link(this.reference.tail, node);
+            this.reference.tail = node;
+        }
+    }
+
+    /**
+     * Add a {@link Node} after a specific node.
+     *
+     * @param before A {@link Node} to insert after.
+     * @param node The {@link Node} to be inserted.
+     */
+    protected void addNodeAfter(Node<T> before, Node<T> node) {
+        assert(this.reference != null);
+
+        Node.link(before, node);
+
+        if (this.reference.tail == before)
+            this.reference.tail = node;
+    }
+
+    /**
+     * Remove the {@link Node} from the list.
+     * Also invalidates head and tail reference if affected.
+     *
+     * @param node The {@link Node} to remove.
+     */
+    protected void removeNode(Node<T> node) {
+        node.unlink();
+
+        assert(this.reference != null);
+
+        if (this.length == 0)
+            this.reference = null;
+
+        else if (this.reference.head == node)
+            this.reference.head = node.next;
+
+        else if (this.reference.tail == node)
+            this.reference.tail = node.before;
+    }
+
+    @Override
+    public void add(T e) {
+        var node = new Node<>(e);
+
+        this.addNodeTail(node);
+        this.length++;
+    }
+
+    @Override
+    public void addLeft(T e) {
+        var node = new Node<>(e);
+
+        this.addNodeHead(node);
+        this.length++;
+    }
+
+    @Override
+    public void insert(int index, T e) throws IndexOutOfBoundsException {
+        this.throwIfOutOfBounds(index);
+
+        var before = this.getNode(index);
+
+        var node = new Node<>(e);
+
+        this.addNodeAfter(before, node);
+        this.length++;
+    }
+
+    @Override
+    public T get(@Range(from = 0, to = Integer.MAX_VALUE) int index) throws IndexOutOfBoundsException {
+        this.throwIfOutOfBounds(index);
+
+        var node = this.getNode(index);
+        return node.data;
+    }
+
+    @Override
+    public @Nullable T getFirst() {
+        if (this.reference == null) return null;
+
+        return this.reference.head.data;
+    }
+
+    @Override
+    public @Nullable T getLast() {
+        if (this.reference == null) return null;
+
+        return this.reference.tail.data;
+    }
+
+    @Override
+    public void remove(@Range(from = 0, to = Integer.MAX_VALUE) int index) throws IndexOutOfBoundsException {
+        this.throwIfOutOfBounds(index);
+
+        var node = this.getNode(index);
+
+        this.removeNode(node);
+        this.length--;
+    }
+
+    @Override
+    public T pop() throws IndexOutOfBoundsException {
+        if (this.reference == null) throw new IndexOutOfBoundsException();
+
+        var node = this.reference.tail;
+
+        this.removeNode(node);
+        this.length--;
+
+        return node.data;
+    }
+
+    @Override
+    public T popFirst() throws IndexOutOfBoundsException {
+        if (this.reference == null) throw new IndexOutOfBoundsException();
+
+        var node = this.reference.head;
+
+        this.removeNode(node);
+        this.length--;
+
+        return node.data;
+    }
+
+    @Override
+    public void clear() {
+        this.reference = null;
+        this.length = 0;
+    }
+
+    @Override
+    public @Range(from = 0, to = Integer.MAX_VALUE) int size() {
+        return this.length;
+    }
+
+    @Override
+    public @Nullable T findFirst(Filter<T> filter) {
+        for (var data : this)
+            if (filter.filter(data))
+                return data;
+
+        return null;
+    }
+
+    @Override
+    public @Range(from = 0, to = Integer.MAX_VALUE) int index(Filter<T> filter) throws NoSuchElementException {
+        var index = 0;
+        for (var data : this)
+            if (filter.filter(data))
+                return index;
+            else
+                index++;
+
+        throw new NoSuchElementException();
+    }
+
+    @Override
+    public void extend(ListInterface<T> other) {
+        for (var data : other) {
+            var node = new Node<>(data);
+
+            this.addNodeTail(node);
+            this.length++;
+        }
+    }
+
+    @Override
+    public void filter(Filter<T> filter) {
+        if (this.reference == null) return;
+
+        for (var node : this.reference.head)
+            if (!filter.filter(node.data)) {
+                this.removeNode(node);
+                this.length--;
+            }
+    }
+
+    @Override
+    public void sort(Comparator<T> sorter) {
+        // TODO: Implement sorting
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public ListInterface<T> filtered(Filter<T> filter) {
+        DoubleLinkedList<T> clone;
+        clone = (DoubleLinkedList<T>) this.clone();
+
+        clone.filter(filter);
+
+        return clone;
+    }
+
+    @Override
+    public ListInterface<T> sorted(Comparator<T> sorter) {
+        DoubleLinkedList<T> clone;
+        clone = (DoubleLinkedList<T>) this.clone();
+
+        clone.sort(sorter);
+
+        return clone;
+
+    }
+
+    @Override
+    public @NotNull Iterator<T> iterator() {
+        return new LinkedListIterator<>(this);
+    }
+
+    @Override
+    @SuppressWarnings("MethodDoesntCallSuperMethod")
+    public ListInterface<T> clone() {
+        var clone = new DoubleLinkedList<T>();
+        clone.extend(this);
+
+        return clone;
+    }
+
+    protected static class LinkedListIterator<K> implements Iterator<K>, Iterable<K> {
+        private final Node.NodeIterator<K> iterator;
+
+        public LinkedListIterator(Node.NodeIterator<K> nodeIterator) {
             this.iterator = nodeIterator;
         }
 
         public LinkedListIterator(Node<K> node) {
-            this(new NodeIterator<>(node));
+            this(new Node.NodeIterator<>(node));
+        }
+
+        public LinkedListIterator(DoubleLinkedList<K> linkedList) {
+            this(linkedList.reference != null ? linkedList.reference.head : null);
         }
 
         @Override
@@ -103,234 +418,5 @@ public class DoubleLinkedList<T> implements ListInterface<T> {
         public @NotNull Iterator<K> iterator() {
             return this;
         }
-    }
-
-    protected void throwIfOutOfBounds(@Range(from = 0, to = Integer.MAX_VALUE) int index) throws IndexOutOfBoundsException {
-        if (index >= this.length)
-            throw new IndexOutOfBoundsException();
-    }
-
-    /**
-     * Reset this.last to the actual last node and this.length to the new length.
-     */
-    protected void resetLastNode() {
-        if (this.first == null) {  // if there's no first, there's no last
-            this.length = 0;
-            return;
-        }
-
-        // if there is no this.last node, start at this.first node, else start at this.last node
-        Node<T> startingNode;
-        if (this.last != null) startingNode = this.last;
-        else {
-            startingNode = this.first;
-            this.length = 1;
-        }
-
-        // traverse and find the node without .next and designates it as this.last node
-        for (var node : new NodeIterator<>(startingNode))
-            if (node.next == null) this.last = node;
-            else this.length++;
-    }
-
-    protected Node<T> getNode(int index) throws IndexOutOfBoundsException {
-        this.throwIfOutOfBounds(index);
-
-        var i = 0;
-        for (var node : this.getNodeIterator())
-            if (i == index)
-                return node;
-            else
-                i++;
-
-        throw new IndexOutOfBoundsException();
-    }
-
-    protected void addNode(Node<T> node) {
-        if (this.first != null)  // this.first being set usually means this.last is also set
-            this.addNode(this.last, node);
-        else {
-            this.first = node;
-            this.last = node;
-            this.length = 1;
-        }
-    }
-
-    protected void addNode(Node<T> at, Node<T> node) {
-        Node.chain(at, node);
-        this.resetLastNode();
-    }
-
-    protected void removeNode(Node<T> node) {
-        node.destroy();
-
-        if (this.first == node) this.first = this.first.next;
-        else if (this.last == node) this.last = this.last.before;
-
-        this.length--;
-    }
-
-    protected @Nullable Node<T> findNode(Filter<T> filter) {
-        for (var node : this.getNodeIterator())
-            if (filter.filter(node.data))
-                return node;
-
-        return null;
-    }
-
-    protected int indexNode(Filter<T> filter) throws NoSuchElementException {
-        var i = 0;
-        for (var node : this.getNodeIterator())
-            if (filter.filter(node.data))
-                return i;
-            else
-                i++;
-
-        throw new NoSuchElementException();
-    }
-
-    protected NodeIterator<T> getNodeIterator() {
-        return new NodeIterator<>(this.first);
-    }
-
-    @Override
-    public void add(T e) {
-        var node = new Node<T>();
-        node.data = e;
-
-        this.addNode(node);
-    }
-
-    @Override
-    public void insert(int index, T e) {
-        var insertAt = this.getNode(index);
-
-        var node = new Node<T>();
-        node.data = e;
-
-        this.addNode(insertAt, node);
-    }
-
-    @Override
-    public void remove(@Range(from = 0, to = Integer.MAX_VALUE) int index) throws IndexOutOfBoundsException {
-        this.throwIfOutOfBounds(index);
-
-        var node = this.getNode(index);
-        this.removeNode(node);
-
-        this.length--;
-    }
-
-    @Override
-    public T get(@Range(from = 0, to = Integer.MAX_VALUE) int index) throws IndexOutOfBoundsException {
-        this.throwIfOutOfBounds(index);
-
-        return this.getNode(index).data;
-    }
-
-    @Override
-    public @Nullable T getFirst() {
-        if (this.first != null) return this.first.data;
-        else return null;
-    }
-
-    @Override
-    public @Nullable T getLast() {
-        if (this.last != null) return this.last.data;
-        else return null;
-    }
-
-    @Override
-    public T pop() throws IndexOutOfBoundsException {
-        if (this.last == null) throw new IndexOutOfBoundsException();
-
-        var popped = this.last;
-        this.removeNode(popped);
-
-        return popped.data;
-    }
-
-    @Override
-    public T popFirst() throws IndexOutOfBoundsException {
-        if (this.first == null) throw new IndexOutOfBoundsException();
-
-        var popped = this.first;
-        this.removeNode(popped);
-
-        return popped.data;
-    }
-
-    @Override
-    public void clear() {
-        this.first = null;
-        this.last = null;
-        this.length = 0;
-    }
-
-    @Override
-    public @Range(from = 0, to = Integer.MAX_VALUE) int size() {
-        return this.length;
-    }
-
-    @Override
-    public @Nullable T findFirst(Filter<T> filter) {
-        var node = this.findNode(filter);
-
-        if (node != null) return node.data;
-        else return null;
-    }
-
-    @Override
-    public @Range(from = 0, to = Integer.MAX_VALUE) int index(Filter<T> filter) throws NoSuchElementException {
-        return this.indexNode(filter);
-    }
-
-    @Override
-    public void extend(ListInterface<T> other) {
-        if (other instanceof DoubleLinkedList<T> linkedList) {
-            if (this.first == null) this.first = linkedList.first;
-            else  // assume this.first and this.last is not null
-                Node.chain(this.last, linkedList.first);
-
-            this.length += linkedList.length;
-            this.last = linkedList.last;
-        }
-
-        // TODO: Implement extending non linked lists.
-        throw new UnsupportedOperationException("Not implemented.");
-    }
-
-    @Override
-    public void filter(Filter<T> filter) {
-        for (var node : this.getNodeIterator())
-            if (!filter.filter(node.data)) this.removeNode(node);
-    }
-
-    @Override
-    public void sort(Comparator<T> sorter) {
-        // TODO: Implement sorting
-        throw new UnsupportedOperationException("Not implemented");
-    }
-
-    @Override
-    public ListInterface<T> filtered(Filter<T> filter) {
-        var copy = new DoubleLinkedList<T>();
-
-        for (var node : this.getNodeIterator())
-            if (!filter.filter(node.data))
-                copy.addNode(node);
-
-        return copy;
-    }
-
-    @Override
-    public ListInterface<T> sorted(Comparator<T> sorter) {
-        // TODO: Implement sorting
-        throw new UnsupportedOperationException("Not implemented");
-    }
-
-    @Override
-    public @NotNull Iterator<T> iterator() {
-        return new LinkedListIterator<>(this.first);
     }
 }
--- src/main/java/edu/dsa/clinic/adt/SortedDoubleLinkedList.java
+++ src/main/java/edu/dsa/clinic/adt/SortedDoubleLinkedList.java
@@ -2,6 +2,12 @@ package edu.dsa.clinic.adt;
 
 import java.util.Comparator;
 
+/**
+ * The sorted version of {@link DoubleLinkedList}.
+ *
+ * @param <T> The datatype of the element in the list.
+ * @author dizzynight
+ */
 public class SortedDoubleLinkedList<T> extends DoubleLinkedList<T> {
     protected Comparator<T> sorter;
 
@@ -10,16 +16,27 @@ public class SortedDoubleLinkedList<T> extends DoubleLinkedList<T> {
         this.sorter = sorter;
     }
 
-    @Override
-    protected void addNode(Node<T> newNode) {
-        for (var node : this.getNodeIterator())
-            if (sorter.compare(newNode.data, node.data) > 0)
-                super.addNode(node, newNode);
+    protected void addNodeSorted(Node<T> node) {
+        if (this.reference == null)
+            this.reference = new Reference(node);
+        else {
+            for (var before : this.reference.head)
+                if (sorter.compare(before.data, node.data) > 0)
+                    super.addNodeAfter(before, node);
+        }
     }
 
     @Override
-    protected void addNode(Node<T> at, Node<T> node) {
-        this.addNode(node);
+    public void add(T e) {
+        var node = new Node<>(e);
+
+        this.addNodeSorted(node);
+        this.length++;
+    }
+
+    @Override
+    public void addLeft(T e) {
+        this.add(e);
     }
 
     @Override
@@ -31,25 +48,5 @@ public class SortedDoubleLinkedList<T> extends DoubleLinkedList<T> {
     public void sort(Comparator<T> sorter) {
         this.sorter = sorter;
         super.sort(sorter);
-    }
-
-    @Override
-    public ListInterface<T> filtered(Filter<T> filter) {
-        SortedDoubleLinkedList<T> filteredList = new SortedDoubleLinkedList<>(this.sorter);
-        for (T item : this) {
-            if (filter.filter(item)) {
-                filteredList.add(item);
-            }
-        }
-        return filteredList;
-    }
-
-    @Override
-    public ListInterface<T> sorted(Comparator<T> sorter) {
-        SortedDoubleLinkedList<T> sortedList = new SortedDoubleLinkedList<>(sorter);
-        for (T item : this) {
-            sortedList.add(item);
-        }
-        return sortedList;
     }
 }
