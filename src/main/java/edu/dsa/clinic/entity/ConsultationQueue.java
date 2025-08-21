d5fbfc577ef7d9a8e8173ff6a0a8d4f7fc4155b5
darentks <darentks-wm23@student.tarc.edu.my>
2025-08-21 13:35:51 +0800

    added DoctorUI(unfinished)

---
+++ src/main/java/edu/dsa/clinic/entity/ConsultationQueue.java
@@ -0,0 +1,10 @@
+package edu.dsa.clinic.entity;
+
+public class ConsultationQueue extends IdentifiableEntity{
+    private Consultation consultation;
+    private Doctor doctor;
+    private Patient patient;
+
+    public ConsultationQueue() {}
+
+}
