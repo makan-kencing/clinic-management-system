20a60957b18ccd3e1e04d8d65f9ef926d1abd4d2
WHannnnnnn <whbeh11@gmail.com>
2025-08-21 21:08:18 +0800

    added appointmentList and doctorList

--- src/main/java/edu/dsa/clinic/Database.java
+++ src/main/java/edu/dsa/clinic/Database.java
@@ -2,8 +2,10 @@ package edu.dsa.clinic;
 
 import edu.dsa.clinic.adt.DoubleLinkedList;
 import edu.dsa.clinic.adt.ListInterface;
+import edu.dsa.clinic.entity.Appointment;
 import edu.dsa.clinic.entity.Consultation;
 import edu.dsa.clinic.entity.Diagnosis;
+import edu.dsa.clinic.entity.Doctor;
 import edu.dsa.clinic.entity.Medicine;
 import edu.dsa.clinic.entity.Patient;
 import edu.dsa.clinic.entity.Prescription;
@@ -15,7 +17,10 @@ public class Database {
     public final static ListInterface<Consultation> consultationsList = new DoubleLinkedList<>();
     public final static ListInterface<Medicine> medicineList = new DoubleLinkedList<>();
     public final static ListInterface<Prescription> prescriptionsList =new DoubleLinkedList<>();
-    public final static ListInterface<Treatment> treatmentsList=new DoubleLinkedList<>();
+    public final static ListInterface<Treatment> treatmentsList = new DoubleLinkedList<>();
+    public final static ListInterface<Doctor> doctorList = new DoubleLinkedList<>();
+    public final static ListInterface<Appointment> appointmentList = new DoubleLinkedList<>();
+
     private Database() {
     }
 }
