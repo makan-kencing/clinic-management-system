b365e190765afdf45a3b77088959c0b580313f3b
makan-kencing <dizzynight2096@gmail.com>
2025-08-25 01:08:59 +0800

    feat: add DispensaryUI stubs

--- src/main/java/edu/dsa/clinic/boundary/DispensaryUI.java
+++ src/main/java/edu/dsa/clinic/boundary/DispensaryUI.java
@@ -1,11 +1,29 @@
 package edu.dsa.clinic.boundary;
 
-public class DispensaryUI {
-    public void viewDispenses() {
+import edu.dsa.clinic.control.DispensaryController;
+import org.jline.terminal.Terminal;
+
+import java.util.Scanner;
+
+public class DispensaryUI extends UI {
+
+    public DispensaryUI(Terminal terminal) {
+        super(terminal);
+    }
+
+    public DispensaryUI(Scanner scanner) {
+        super(scanner);
+    }
+
+    public void startMenu() {
 
     }
 
-    public void chooseStockToDispense() {
+    public void viewDispensaryQueue() {
+
+    }
+
+    public void clearDispensing(DispensaryController.DispensaryQueue dispensing) {
 
     }
 }
