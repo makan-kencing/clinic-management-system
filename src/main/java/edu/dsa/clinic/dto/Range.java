bfb3fb740460707c51b00d9612ac9b77d230dbc6
makan-kencing <dizzynight2096@gmail.com>
2025-08-27 10:35:10 +0800

    feat: add subtraction to Range

--- src/main/java/edu/dsa/clinic/dto/Range.java
+++ src/main/java/edu/dsa/clinic/dto/Range.java
@@ -1,6 +1,11 @@
 package edu.dsa.clinic.dto;
 
+import edu.dsa.clinic.adt.DoubleLinkedList;
+import edu.dsa.clinic.adt.ListInterface;
+import edu.dsa.clinic.adt.SortedDoubleLinkedList;
 import edu.dsa.clinic.utils.Ordered;
+
+import java.util.Comparator;
 
 /**
  * A pair of two values usually relating to upper bound and lower bound.
@@ -53,6 +58,17 @@ public record Range<T extends Comparable<T>>(
     }
 
     /**
+     * Check if the other {@link Range} is exclusively contained by this {@link Range}
+     *
+     * @param other The other {@link Range} to check contains.
+     * @return If this {@link Range} contains the given {@link Range}
+     */
+    public boolean containsExclusively(Range<T> other) {
+        return Ordered.isBetweenExclusively(this.from, other.from, other.to)
+                && Ordered.isBetweenExclusively(this.to, other.from, other.to);
+    }
+
+    /**
      * Get the overlapped {@link Range} between two {@link Range}s.
      *
      * @param other The other {@link Range} to overlap.
@@ -76,5 +92,36 @@ public record Range<T extends Comparable<T>>(
                 Ordered.min(this.from, other.from),
                 Ordered.max(this.to, other.to)
         );
+    }
+
+    /**
+     * Subtract this {@link Range} with other {@link Range} exclusively.
+     *
+     * @param other The other {@link Range} to subtract with.
+     * @return The split {@link Range}(s).
+     */
+    // https://stackoverflow.com/questions/6462272/subtract-overlaps-between-two-ranges-without-sets
+    public ListInterface<Range<T>> subtract(Range<T> other) {
+        var ranges = new DoubleLinkedList<Range<T>>();
+
+        var coords = new SortedDoubleLinkedList<T>(Comparator.naturalOrder());
+        coords.add(this.from);
+        coords.add(this.to);
+        coords.add(other.from);
+        coords.add(other.to);
+
+        var iterator = coords.iterator();
+
+        var start = iterator.next();
+        var end = iterator.next();
+        if (start == this.from && end != this.from)
+            ranges.add(new Range<>(start, end));
+
+        start = iterator.next();
+        end = iterator.next();
+        if (end == this.to && start != this.to)
+            ranges.add(new Range<>(start, end));
+
+        return ranges;
     }
 }
